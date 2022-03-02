c0abd29b4067d21a676116bca3facc8ee5e6bb68
Lochlan <lbernard@nzigen.com>
2022-03-02 23:08:07 +0900

    Import work from other flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,34 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
+        canvasColor: Colors.grey[400],
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FirstRoute(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  MyHomePage({required Key key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -52,50 +36,21 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
+            Text(
               'You have pushed the button this many times:',
             ),
             Text(
@@ -108,8 +63,145 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animation Playground'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Dino Route'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => DinoGame()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class DinoGame extends StatefulWidget {
+  @override
+  _DinoGameState createState() => _DinoGameState();
+}
+
+class _DinoGameState extends State<DinoGame> {
+  ScrollController _scrollController = ScrollController();
+
+  _scrollRight() {
+    _scrollController.animateTo(_scrollController.position.maxScrollExtent,
+        duration: Duration(seconds: 90), curve: Curves.easeOut);
+  }
+
+  double dinoYaxis = 1;
+
+  void jump() {
+    setState(() {
+      dinoYaxis -= 1;
+      Timer(Duration(milliseconds: 500), () {
+        setState(() {
+          dinoYaxis = 1;
+        });
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    WidgetsBinding.instance?.addPostFrameCallback((_) => _scrollRight());
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dino Game'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 20.0, 0, 0),
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Text('Dino Here'),
+                    SizedBox(
+                      height: 300,
+                      width: double.infinity,
+                      child: Stack(
+                        alignment: AlignmentDirectional.bottomCenter,
+                        children: [
+                          Stack(
+                            children: [
+                              ListView.builder(
+                                itemCount: 10,
+                                scrollDirection: Axis.horizontal,
+                                controller: _scrollController,
+                                itemBuilder: (content, i) {
+                                  return Image.asset(
+                                    'assets/images/dino1.png',
+                                    fit: BoxFit.fill,
+                                  );
+                                },
+                              ),
+                              SizedBox(
+                                height: 230,
+                                width: double.infinity,
+                                child: ColoredBox(color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                          AnimatedContainer(
+                            curve: Curves.easeInOutSine,
+                            duration: Duration(milliseconds: 500),
+                            alignment: Alignment(0, dinoYaxis),
+                            child: Image.asset(
+                              'assets/images/dinoSprite.png',
+                              fit: BoxFit.fill,
+                              height: 104,
+                              width: 100,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: GestureDetector(onTap: jump, child: Text('Jump')),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Home'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
